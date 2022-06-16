000a0f3ae31fcf5298f1f5c099d52df776ca0328
Shahed-80 <107167352+Shahed-80@users.noreply.github.com>
2022-06-16 11:09:54 +0530

    Add files via upload

---
+++ 06_day_data_types_str.py
@@ -0,0 +1,68 @@
+''' Data types - String '''
+
+# print('hello world')
+
+# print('dfg'.isidentifier())
+
+
+# a = 'I am '
+# u = 'iron man'
+
+# print(a+u)
+
+
+# n = "Shahed"
+# a = 22
+# p = 'Hyd'
+
+# print('My name is',n,'and I am',a,'I am from',p)
+# print('my name is',n + ' my age is',str(a) + ' I am from',p)
+
+# print('my name is '+ n,'and my age is '+ str(a),'I am from '+ p)
+
+# print(type(''))
+# print(type('.'))
+# print(type(','))
+# print(type('tgy'))
+# print(type('   '))
+
+
+d = 'python '
+c = 'program'
+print(d+c)
+
+
+t = 'Develop a'
+x = ' software'
+
+print(t,x)
+print(t+x)
+
+print('Develop a ','software')
+
+
+print(''.isspace())
+print(','.isspace())       # Any thing inside the string or empty str will give us False
+print('hyd'.isspace())
+print('....'.isspace())
+print('           '.isspace())    # Single or multiple Space inside the str will give us True
+print(' I want to be'.isspace())
+
+
+a = "Mohammad"
+b = '22'
+c = 'Graduation'
+d = '7.2'
+
+
+print('My name is '+ a+'.')
+print('My age is '+b+'.')
+print('I have done my '+c+'.')
+print('And my grade is '+d+'.')
+
+print('My name is' +' '+ a +'.' +'My age is'+' ' +b+'.'  +'I have done my' +' ' + c +'.'  +'And my grade is'+d)
+
+print('hello' 'world')
+print('hello' , 'world')
+print('hello' +' ' + 'world')
+print('hello'  + 'world')
